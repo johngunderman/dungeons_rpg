9f9e64b59f41e2f91215de16dc757b2a92f33a1b
John Gunderman <johngunderman@gmail.com>
2009-03-16 21:48:51 -0700

    basic random encounter functionality added.

--- player.py
+++ player.py
@@ -1,9 +1,20 @@
 
 from actor import Actor
+import random
+import pygame
 
 class Player (Actor):
 
     def __init__(self, dimensions, position, screen=None):
         super(Player, self).__init__(dimensions, position, screen)
 
-
+    def move(self, position):
+        """Overrides actor so that we get random encounter generation."""
+        #TODO: We need to limit random encounters to only certain areas on map.
+        super(Player, self).move(position)
+        event = random.randint(0,20)
+        if event == 0:
+            self.random_encounter()
+            
+    def random_encounter(self):
+        print "RANDOM ENCOUNTER!!!"
